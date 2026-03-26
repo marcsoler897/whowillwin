be2dcdfa0128b8979d4e7f8d40dfe99f7127b045
Marc Soler Martí <marcsoler897@gmail.com>
2026-03-26 17:47:23 +0100

    Added Email to db.sql

--- Backend/db/db.sql
+++ Backend/db/db.sql
@@ -9,7 +9,11 @@ CREATE TABLE whowillwin.users (
     id UUID PRIMARY KEY,
     prefteam_id UUID NOT NULL,
     name VARCHAR(32) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     CONSTRAINT fk_user_team FOREIGN KEY (prefteam_id)
         REFERENCES whowillwin.teams(id)
-);
+); 
+
+INSERT INTO teams (id, name)
+VALUES (gen_random_uuid(), 'Random Team');
