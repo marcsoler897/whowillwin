3996c29749726371e16a22bc20d3b05718bb140d
Marc Soler Martí <marcsoler897@gmail.com>
2026-04-24 16:38:55 +0200

    Basic scikit-learn implementation

--- Predictor/model.py
+++ Predictor/model.py
@@ -21,7 +21,20 @@ away_stats = df.groupby("away_team_id")["away_goals"].mean()
 home_wins = df.groupby("home_team_id")["result"].apply(lambda x: (x == 0).mean())
 away_wins = df.groupby("away_team_id")["result"].apply(lambda x: (x == 2).mean())
 
-print(home_wins)
-print(away_wins)
+df["home_goals_per_game"] = df["home_team_id"].map(home_stats)
+df["away_goals_per_game"] = df["away_team_id"].map(away_stats)
+df["home_win_rate"] = df["home_team_id"].map(home_wins)
+df["away_win_rate"] = df["away_team_id"].map(away_wins)
 
-# print(df)
+X = df[["home_goals_per_game", "away_goals_per_game", "home_win_rate", "away_win_rate"]]
+y = df["result"]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+clf = LogisticRegression(random_state=0).fit(X_train, y_train)
+print(clf.predict(X_test[:5]))
+print(clf.predict_proba(X_test[:5]))
+print(clf.score(X_test, y_test))
