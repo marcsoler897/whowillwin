9726519fe63b1894fe2c2d0391d5ebb5468695e3
Marc Soler Martí <marcsoler897@gmail.com>
2026-04-24 16:12:28 +0200

    Converting data

--- Predictor/model.py
+++ Predictor/model.py
@@ -1,11 +1,27 @@
 import pandas as pd
-import psycopg2
+from sqlalchemy import create_engine
+import numpy as np
 
-conn = psycopg2.connect(
-    host="localhost", port=5432,
-    user="postgres", password="postgres", dbname="postgres"
-)
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+df = pd.read_sql("SELECT * FROM whowillwin.matches", engine)
 
-df = pd.read_sql("SELECT * FROM whowillwin.matches", conn)
+df.to_csv("matches.csv", index=False)
 
-print(df)
+df = df[["season", "matchday", "utc_date", "home_team_id", "away_team_id", "home_goals", "away_goals", "winner"]]
+df = df.dropna(subset=["home_goals", "away_goals", "winner"])
+
+
+df["result"] = df["winner"].map({"HOME_TEAM": 0, "DRAW": 1, "AWAY_TEAM": 2})
+
+
+df.groupby("home_team_id")["home_goals"].mean()
+home_stats = df.groupby("home_team_id")["home_goals"].mean()
+away_stats = df.groupby("away_team_id")["away_goals"].mean()
+
+home_wins = df.groupby("home_team_id")["result"].apply(lambda x: (x == 0).mean())
+away_wins = df.groupby("away_team_id")["result"].apply(lambda x: (x == 2).mean())
+
+print(home_wins)
+print(away_wins)
+
+# print(df)
